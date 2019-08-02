0aebbca81557cae9d53b2e790a0845c83d1d0efc
Mark Piper <mark.piper@colorado.edu>
2019-08-02 11:21:48 -0600

    wip: Get variable and grid information
    Note that *get_grid_[x,y,z]* are being used in a nonstandard manner;
    however, this is an issue with BMI, not this implementation. We need
    a better way to handle grids that are rectangular, but with uneven
    spacing.

--- examples/gipl_bmi_ex.py
+++ examples/gipl_bmi_ex.py
@@ -1,5 +1,6 @@
 """An example of running GIPL through its Python-wrapped BMI."""
 
+import numpy as np
 from pymt_gipl import GIPL
 
 
@@ -24,6 +25,42 @@ print('End time:', m.get_end_time())
 print('Current time:', m.get_current_time())
 print('Time step:', m.get_time_step())
 print('Time units:', m.get_time_units())
+
+# Get the grid_id for the soil__temperature variable.
+var_name = 'soil__temperature'
+print('Variable: {}'.format(var_name))
+grid_id = m.get_var_grid(var_name)
+print(' - grid id:', grid_id)
+
+# Get grid and variable info for WaterSurfaceElevation.
+print(' - grid type:', m.get_grid_type(grid_id))
+grid_rank = m.get_grid_rank(grid_id)
+print(' - rank:', grid_rank)
+grid_shape = np.empty(grid_rank, dtype=np.int32)
+m.get_grid_shape(grid_id, grid_shape)
+print(' - shape:', grid_shape)
+grid_size = m.get_grid_size(grid_id)
+print(' - size:', grid_size)
+print(' - spacing: n/a')
+grid_origin = np.empty(grid_rank, dtype=np.float64)
+m.get_grid_origin(grid_id, grid_origin)
+print(' - origin:', grid_origin)
+# grid_x = np.empty(grid_size, dtype=np.float64)
+grid_x = np.empty(grid_shape[0], dtype=np.float64)
+m.get_grid_x(grid_id, grid_x)
+print(' - x nodes:', grid_x)
+# grid_y = np.empty(grid_size, dtype=np.float64)
+grid_y = np.empty(grid_shape[1], dtype=np.float64)
+m.get_grid_y(grid_id, grid_y)
+print(' - y nodes:', grid_y)
+# grid_z = np.empty(grid_size, dtype=np.float64)
+grid_z = np.empty(grid_shape[2], dtype=np.float64)
+m.get_grid_z(grid_id, grid_z)
+print(' - z nodes:', grid_z)
+print(' - variable type:', m.get_var_type(var_name))
+print(' - units:', m.get_var_units(var_name))
+print(' - itemsize:', m.get_var_itemsize(var_name))
+print(' - nbytes:', m.get_var_nbytes(var_name))
 
 # Finalize the model.
 m.finalize()
