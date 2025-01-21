d2a81d4ac96e6be0ea99196968e42c49dd91cd8b
Mark Piper <mark.piper@colorado.edu>
2025-01-21 14:51:36 -0700

    Make pretty

--- examples/gipl_bmi_ex.py
+++ examples/gipl_bmi_ex.py
@@ -9,88 +9,88 @@ m = GIPL()
 print(m.get_component_name())
 
 # List input and output variable names.
-print('Input variable names:')
+print("Input variable names:")
 for name in m.get_input_var_names():
-    print(' - {}'.format(name))
-print('Output variable names:')
+    print(" - {}".format(name))
+print("Output variable names:")
 for name in m.get_output_var_names():
-    print(' - {}'.format(name))
+    print(" - {}".format(name))
 
 # Initialize the model with a config file pointing to local files.
-m.initialize('gipl_config.cfg')
+m.initialize("gipl_config.cfg")
 
 # Get time information from the model.
-print('Start time:', m.get_start_time())
-print('End time:', m.get_end_time())
-print('Current time:', m.get_current_time())
-print('Time step:', m.get_time_step())
-print('Time units:', m.get_time_units())
+print("Start time:", m.get_start_time())
+print("End time:", m.get_end_time())
+print("Current time:", m.get_current_time())
+print("Time step:", m.get_time_step())
+print("Time units:", m.get_time_units())
 
 # Get the grid_id for the soil__temperature variable.
-var_name = 'soil__temperature'
-print('Variable: {}'.format(var_name))
+var_name = "soil__temperature"
+print("Variable: {}".format(var_name))
 grid_id = m.get_var_grid(var_name)
-print(' - grid id:', grid_id)
+print(" - grid id:", grid_id)
 
 # Get grid and variable info for soil__temperature.
-print(' - grid type:', m.get_grid_type(grid_id))
+print(" - grid type:", m.get_grid_type(grid_id))
 grid_rank = m.get_grid_rank(grid_id)
-print(' - rank:', grid_rank)
+print(" - rank:", grid_rank)
 grid_shape = np.empty(grid_rank, dtype=np.int32)
 m.get_grid_shape(grid_id, grid_shape)
-print(' - shape:', grid_shape)
+print(" - shape:", grid_shape)
 grid_size = m.get_grid_size(grid_id)
-print(' - size:', grid_size)
-print(' - spacing: n/a for a {} grid'.format(m.get_grid_type(grid_id)))
+print(" - size:", grid_size)
+print(" - spacing: n/a for a {} grid".format(m.get_grid_type(grid_id)))
 grid_origin = np.empty(grid_rank, dtype=np.float64)
 m.get_grid_origin(grid_id, grid_origin)
-print(' - origin:', grid_origin)
-print(' - x nodes: n/a')
-print(' - y nodes: n/a')
+print(" - origin:", grid_origin)
+print(" - x nodes: n/a")
+print(" - y nodes: n/a")
 grid_z = np.empty(grid_shape, dtype=np.float64)
 m.get_grid_z(grid_id, grid_z)
-print(' - z nodes:', grid_z)
-print(' - variable type:', m.get_var_type(var_name))
-print(' - units:', m.get_var_units(var_name))
-print(' - itemsize:', m.get_var_itemsize(var_name))
-print(' - nbytes:', m.get_var_nbytes(var_name))
+print(" - z nodes:", grid_z)
+print(" - variable type:", m.get_var_type(var_name))
+print(" - units:", m.get_var_units(var_name))
+print(" - itemsize:", m.get_var_itemsize(var_name))
+print(" - nbytes:", m.get_var_nbytes(var_name))
 
 # Advance the model by one time step.
-print('Updating model...')
+print("Updating model...")
 m.update()
-print(' - current time:', m.get_current_time())
+print(" - current time:", m.get_current_time())
 
 # Advance the model by a fractional time step.
-print('Updating model...')
+print("Updating model...")
 m.update_frac(0.75)
-print(' - current time:', m.get_current_time())
+print(" - current time:", m.get_current_time())
 
 # Advance the model until a later time.
-print('Updating model...')
+print("Updating model...")
 m.update_until(15.0)
-print(' - current time:', m.get_current_time())
+print(" - current time:", m.get_current_time())
 
 # Get the soil__temperature values.
-print('Get values for {}:'.format(var_name))
+print("Get values for {}:".format(var_name))
 val = np.empty(grid_shape, dtype=np.float32)
 m.get_value(var_name, val)
-print(' - values (streamwise):')
+print(" - values (streamwise):")
 print(val)
 
 # The get_value_ptr method isn't implemented.
 
 # Set new soil__temperature values.
-print('Set new values for {}:'.format(var_name))
+print("Set new values for {}:".format(var_name))
 new = np.arange(grid_shape, dtype=np.float32)
-print(' - new values (streamwise):')
+print(" - new values (streamwise):")
 print(new)
 m.set_value(var_name, new)
 check = np.empty(grid_size, dtype=np.float32)
 m.get_value(var_name, check)
-print(' - check new values (set/get, streamwise):')
+print(" - check new values (set/get, streamwise):")
 print(check)
-print(' - are new and check the same? (True)', np.array_equal(new, check))
+print(" - are new and check the same? (True)", np.array_equal(new, check))
 
 # Finalize the model.
 m.finalize()
-print('Done.')
+print("Done.")
--- examples/gipl_pymt_ex.py
+++ examples/gipl_pymt_ex.py
@@ -8,46 +8,46 @@ m = GIPL()
 print(m.name)
 
 # List input and output variable names.
-print('Input variable names:')
+print("Input variable names:")
 for name in m.input_var_names:
-    print(' - {}'.format(name))
-print('Output variable names:')
+    print(" - {}".format(name))
+print("Output variable names:")
 for name in m.output_var_names:
-    print(' - {}'.format(name))
+    print(" - {}".format(name))
 
 # Call setup to get default config and data files.
-defaults = m.setup('.')
-print('Setup:')
+defaults = m.setup(".")
+print("Setup:")
 for item in defaults:
-    print(' - {}'.format(item))
+    print(" - {}".format(item))
 
 # Initialize the model with the defaults.
 m.initialize(*defaults)
 
 # Display the start, end, and current model time.
-print('Start time: {}'.format(m.start_time))
-print('End time: {}'.format(m.end_time))
-print('Current time: {}'.format(m.time))
+print("Start time: {}".format(m.start_time))
+print("End time: {}".format(m.end_time))
+print("Current time: {}".format(m.time))
 
 # Update the model state.
-print('Update the model state...')
+print("Update the model state...")
 m.update()
-print('Current time: {}'.format(m.time))
+print("Current time: {}".format(m.time))
 
 # Get the soil temperature at this time.
-soilt = m.var['soil__temperature'].data
-units = m.var['soil__temperature'].units
-print('Soil temperature: {} {}'.format(soilt, units))
+soilt = m.var["soil__temperature"].data
+units = m.var["soil__temperature"].units
+print("Soil temperature: {} {}".format(soilt, units))
 
 # Advance the model to the end.
-print('Run the model to the end...')
+print("Run the model to the end...")
 m.update_until(m.end_time)
-print('Current time: {}'.format(m.time))
+print("Current time: {}".format(m.time))
 
 # Get the final soil temperature distribution.
-soilt = m.var['soil__temperature'].data
-print('Soil temperature: {} {}'.format(soilt, units))
+soilt = m.var["soil__temperature"].data
+print("Soil temperature: {} {}".format(soilt, units))
 
 # Finalize the model.
 m.finalize()
-print('Done.')
+print("Done.")
